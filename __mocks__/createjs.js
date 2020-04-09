808b3554ddf418909583753820189c8f7d089cf4
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-04-09 10:35:11 +0200

    Add tests for Instructions

--- __mocks__/createjs.js
+++ __mocks__/createjs.js
@@ -1,4 +1,6 @@
 export const mockAddChild = jest.fn();
+export const mockUpdate = jest.fn();
+export const mockedStageConstructor = jest.fn();
 export const mockedContainerConstructor = jest.fn();
 export const mockedSpriteConstructor = jest.fn();
 export const spriteSheetConstructorMock = jest.fn();
@@ -26,4 +28,11 @@ export class Bitmap{
 	constructor(data) {
 		return mockedBitmapConstructor(data)
 	}
+}
+export class Stage{
+	constructor(data) {
+		return mockedStageConstructor(data)
+	}
+	addChild = mockAddChild
+	update = mockUpdate
 }
--- src/Instructions/Instructions.test.js
+++ src/Instructions/Instructions.test.js
@@ -1,12 +1,20 @@
 import Instructions from './Instructions'
 import {
-  Bitmap,
+  Stage,
   mockedBitmapConstructor,
+  mockAddChild,
+  mockUpdate,
 } from 'createjs'
 
-test('instructions are rendered when initialized', () => {
-  //spriteSheetConstructorMock.mockImplementation(() => 'spriteSheet') TODO I havent been able to do this
-  //const instructions = new Instructions('image')
+test('instructions are loaded and rendered when initialized', () => {
+  var preloadMock = {
+    getResult : jest.fn(() => 'instructionsImage')
+  }
+  const instructions = new Instructions(new Stage(), preloadMock)
+  expect(mockedBitmapConstructor).toHaveBeenCalledWith('instructionsImage')
+  expect(mockedBitmapConstructor).toHaveBeenCalledTimes(1);
+  expect(instructions.image.x).toBe(0);
+  expect(instructions.image.y).toBe(0);
+  expect(mockAddChild).toHaveBeenCalledTimes(1)
+  expect(mockUpdate).toHaveBeenCalledTimes(1)
 });
-
-//TODO: There are tests missing here!

79a05ab2ed8c0133e628643584cb3f9e5cd77bdf
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-04-13 14:03:24 +0200

    Write the foundations for the level tests

--- src/Factory/__mocks__/Factory.js
+++ src/Factory/__mocks__/Factory.js
@@ -3,11 +3,18 @@ export const loadMock = jest.fn()
 export const getProgressMock = jest.fn()
 export const updateMock = jest.fn()
 export const updateStageMock = jest.fn()
+export const thereIsACollisionMock = jest.fn()
+export const isOutOfBoundsMock = jest.fn()
+export const addChildMock = jest.fn()
 export const startLevelMock = jest.fn()
+export const getResultMock = jest.fn()
+export const areSurpassedMock = jest.fn()
+export const tickMock = jest.fn()
 export const preloader = jest.fn().mockImplementation(() => {
     return {
         load: loadMock,
         getProgress: getProgressMock,
+        getResult: getResultMock,
     }
 })
 export const instructions = jest.fn().mockImplementation(() => {
@@ -26,11 +33,30 @@ export const loadingText = jest.fn().mockImplementation(() => {
 export const stage = jest.fn().mockImplementation(() => {
     return {
         update: updateStageMock,
+        addChild: addChildMock,
     }
 })
-export const background = (cloudsImage, floorImage) => ({})
-export const sonic = (sonicImage) => ({})
-export const enemies = (stage, enemiesImage) => ({})
-export const collisionManager = (sonic, enemies) => ({})
+export const background = jest.fn().mockImplementation(() => {
+    return {
+        tick: tickMock,
+    }
+})
+export const sonic = jest.fn().mockImplementation(() => {
+    return {
+        tick: tickMock,
+        isOutOfBounds: isOutOfBoundsMock,
+    }
+})
+export const enemies = jest.fn().mockImplementation(() => {
+    return {
+        tick: tickMock,
+        areSurpassed: areSurpassedMock,
+    }
+})
+export const collisionManager = jest.fn().mockImplementation(() => {
+    return {
+        thereIsACollision: thereIsACollisionMock,
+    }
+})
 export const score = (scoreImage) => ({})
 export const text = (message, stage, canvas) => ({})
--- src/Level/Level.test.js
+++ src/Level/Level.test.js
@@ -2,10 +2,21 @@ import Level from './Level'
 jest.mock('../Factory/Factory')
 import { preloader as preloaderFactory } from '../Factory/Factory'
 
-test('this tests need to be written', () => {
+test('should start all entities when starting level', () => {
+    const level = initializeLevel()
+    level.start()
+})
+
+test('should update all entities when updating level', () => {
+    const level = initializeLevel()
+    level.start()
+    level.tick()
+})
+
+const initializeLevel = () => {
     const onScore = () => {}
     const onDie = () => {}
     const canvas = document.createElement('canvas')
     const preloader = preloaderFactory()
-    new Level(canvas, preloader, onScore, onDie)
-})
+    return new Level(canvas, preloader, onScore, onDie)
+}

bbb0904fc49fb140fdd76c057506ccfd54f5516e
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-04-13 19:05:50 +0200

    Test another flow

--- src/Collision/Bounds.test.js
+++ src/Collision/Bounds.test.js
@@ -1,8 +1,20 @@
 import Bounds from './Bounds'
+
 test('should calculate bounds', () => {
     const result = new Bounds()
     expect(result.x).toBe(Infinity)
     expect(result.y).toBe(Infinity)
+    expect(result.x2).toBe(-Infinity)
+    expect(result.y2).toBe(-Infinity)
+    expect(result.width).toBe(0)
+    expect(result.height).toBe(0)
+})
+
+test('should replaceInfinitiesWithZeroes', () => {
+    const result = new Bounds()
+    result.replaceInfinitiesWithZeroes()
+    expect(result.x).toBe(0)
+    expect(result.y).toBe(0)
     expect(result.x2).toBe(-Infinity)
     expect(result.y2).toBe(-Infinity)
     expect(result.width).toBe(0)
